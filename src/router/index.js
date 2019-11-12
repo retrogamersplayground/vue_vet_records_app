fb20999a2c45071d3e20cb5cd899852cae440a23
Neil Floyd <neilfloyd99@gmail.com>
2019-11-11 22:59:08 -0500

    authentication added

--- src/router/index.js
+++ src/router/index.js
@@ -7,47 +7,127 @@ import EditMare from '@/components/EditMare'
 import AddHealthRecord from '@/components/AddHealthRecord'
 import ViewHealthRecord from '@/components/ViewHealthRecord'
 import ViewHealthRecord2 from '@/components/ViewHealthRecord2'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import firebase from 'firebase'
 
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
       name: 'dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        requiresGuest: true
+      }
     },
     {
       path: '/add',
       name: 'add-mare',
-      component: AddMare
+      component: AddMare,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/edit/:mare_id',
       name: 'edit-mare',
-      component: EditMare
+      component: EditMare,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/:mare_id',
       name: 'view-mare',
-      component: ViewMare
+      component: ViewMare,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
     path: '/:mare_id',
       name: 'view-health-record',
-      component: ViewHealthRecord
+      component: ViewHealthRecord,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/:mare_id',
         name: 'view-health-record2',
-        component: ViewHealthRecord2
+        component: ViewHealthRecord2,
+        meta: {
+          requiresAuth: true
+        }
       },
     {
       path: '/:mare_id',
       name: 'add-health-record',
-      component: AddHealthRecord
+      component: AddHealthRecord,
+      meta: {
+        requiresAuth: true
+      }
     }
-    
   ]
 })
+
+
+// Nav Guards
+
+router.beforeEach((to, from, next) => {
+// Check for requiredAuth guard
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    //Check if NOT logged in
+    if(!firebase.auth().currentUser) {
+      //Go to login
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      //Proceed to route
+      next()  
+    }
+  } else if(record => record.meta.requiresGuest) {
+    //Check if logged in
+    if(firebase.auth().currentUser) {
+      //Go to login
+      next({
+        path: '/',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+    } else {
+      //Proceed to route
+      next()  
+    }
+  } else {
+    //Proceed to route
+    next()
+  }
+})
+
+export default router
