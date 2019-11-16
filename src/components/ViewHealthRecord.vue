fcc68bf3827a54f466cd343daddc26570484dbcd
Neil Floyd <neilfloyd99@gmail.com>
2019-11-15 23:17:12 -0500

    ViewHealthRecord loop is getting all health records but for all mares

--- src/components/ViewHealthRecord.vue
+++ src/components/ViewHealthRecord.vue
@@ -1,83 +1,59 @@
+
 <template>
-    <div id="view-health-record">
-        
+    <div id="health_record">
         
         <ul class="collection with-header">
             <li class="collection-header">
-                Mare Names: <h4>{{name}}</h4>
+                <h3>Health Records</h3>
             </li>
-            <li v-for="mare in mares" v-bind:key="mare.id">
-                <h5>Mare ID#: {{mare_id}}</h5>
-                <h5>Date: {{hr_date}}</h5>
-                <h5>Deworm: {{deworm}}</h5>
-                <h5>Vaccination: {{vaccination}}</h5>
-                <h5>Comments: {{hr_comments}}</h5>
+            <li v-for="horse_health in horse_health" v-bind:key="horse_health.id" class="collection-item">
+                
+                <h5>Mare ID#: {{horse_health.mare_id}}</h5>
+                <h5>Mare Name: {{horse_health.name}}</h5>
+                <ul>
+                    <li>Mare ID#: {{horse_health.mare_id}}</li>
+                    <li>Date: {{horse_health.hr_date}}</li>
+                    <li>Deworm: {{horse_health.deworm}}</li>
+                    <li>Vaccination: {{horse_health.vaccination}}</li>
+                    <li>Comments: {{horse_health.hr_comments}}</li>
+                </ul>
             </li>
         </ul>
-        <button @click="deleteHealthRecord" class="btn-floating btn-large red"><i class="fa fa-times"></i></button>
-        <div class="fixed-action-btn">
-            <router-link to="/" class="btn-floating btn-large grey"><i class="fa fa-arrow-left"></i></router-link>
-       </div>
     </div>
 </template>
 
 <script>
+
 import db from './firebaseInit'
 export default {
     name: 'view-health-record',
     data () {
         return {
-            mare_id: null,
-            name: null,
-            hr_date: null,
-            deworm: null,
-            vaccination: null,
-            hr_comments: null
+            horse_health: []
         }
     },
-    beforeRouteEnter(to, from, next) {
-        db.collection('horse_health').where('mare_id', '==', to.params.mare_id).get()
-        .then(querySnapshot => {
+    created () {
+        
+        db.collection('horse_health').where('mare_id', '==', to.params.mare_id).orderBy('mare_id').get().then(querySnapshot => {
             querySnapshot.forEach(doc => {
-                next(vm => {
-                    vm.mare_id = doc.data().mare_id
-                    vm.name = doc.data().name
-                    vm.hr_date = doc.data().hr_date
-                    vm.deworm = doc.data().deworm
-                    vm.vaccination = doc.data().vaccination
-                    vm.hr_comments = doc.data().hr_comments
-                })
+                
+                const data = {
+                    'id': doc.id,
+                    'mare_id': doc.data().mare_id,
+                    'name': doc.data().name,
+                    'hr_date': doc.data().hr_date,
+                    'deworm': doc.data().deworm,
+                    'vaccination': doc.data().vaccination,
+                    'hr_comments': doc.data().hr_comments
+
+                }
+                this.horse_health.push(data)
             })
         })
     },
-    watch: {
-        '$route': 'fetchData'
-    },
     methods: {
-        fetchData () {
-            db.collection('horse_health').where('mare_id', '==', this.$route.params.mare_id).get()
-            .then(querySnapshot => {
-                querySnapshot.forEach(doc => {
-                    this.mare_id = doc.data().mare_id
-                    this.name = doc.data().name
-                    this.hr_date = doc.data().hr_date
-                    this.deworm = doc.data().deworm
-                    this.vaccination = doc.data().vaccination
-                    this.hr_comments = doc.data().hr_comments
-                })
-            })
-        },
-        deleteHealthRecord () {
-            if(confirm('Are you sure?')) {
-                db.collection('horse_health').where('mare_id', '==', this.$route.params.mare_id).get()
-                .then(querySnapshot => {
-                    querySnapshot.forEach(doc => {
-                        doc.ref.delete()
-                        this.$router.push('/')
-                    })
-                })
-            }
-        }
-    }
+      
+    }       
+    
 }
 </script>
