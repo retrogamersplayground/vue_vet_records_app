4713a1640025266cd09f0763aa86f3aaddf53dca
Neil Floyd <neilfloyd99@gmail.com>
2020-02-10 22:08:49 -0500

    reset Payment.vue script

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -21,34 +21,34 @@ export default {
         }
     },
     mounted () {
-        (function() {
-        var stripe = Stripe('pk_test_rmvxS3TrkGyvqF5nIitvGu8L00PK5JMuRs');
+   (function() {
+  var stripe = Stripe('pk_test_rmvxS3TrkGyvqF5nIitvGu8L00PK5JMuRs');
 
-        var checkoutButton = document.getElementById('checkout-button-plan_GhtXUAapGzZiKS');
-        checkoutButton.addEventListener('click', function () {
-            //When the customer clicks on the button, redirect
-            //them to Checkout.
-            stripe.redirectToCheckout({
-            items: [{plan: 'plan_GhtXUAapGzZiKS', quantity: 1}],
+  var checkoutButton = document.getElementById('checkout-button-plan_GhtXUAapGzZiKS');
+  checkoutButton.addEventListener('click', function () {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe.redirectToCheckout({
+      items: [{plan: 'plan_GhtXUAapGzZiKS', quantity: 1}],
 
-            //Do not rely on the redirect to the successUrl for fulfilling
-            //purchases, customers may not always reach the success_url after
-            //a successful payment.
-            //Instead use one of the strategies described in
-            //https://stripe.com/docs/payments/checkout/fulfillment
-            successUrl: window.location.protocol + 'https://hopeful-meitner-cb550e.netlify.com/#/',
-            cancelUrl: window.location.protocol + 'https://hopeful-meitner-cb550e.netlify.com/landing',
-            })
-            .then(function (result) {
-            if (result.error) {
-                // If `redirectToCheckout` fails due to a browser or network
-                // error, display the localized error message to your customer.
-                var displayError = document.getElementById('error-message');
-                displayError.textContent = result.error.message;
-            }
-            });
-        });
-        })();
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: window.location.protocol + '//simplemarerecords.com/farm',
+      cancelUrl: window.location.protocol + '//simplemarerecords.com/landing',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  });
+})();
     }
 }
 </script>
