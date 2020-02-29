a55000672d0f19aa70fd78ec66ce402ffb1b6b55
Neil Floyd <neilfloyd99@gmail.com>
2020-02-28 23:22:17 -0500

    correct redirect url test 2

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -81,8 +81,8 @@ export default {
         // a successful payment.
         // Instead use one of the strategies described in
         // https://stripe.com/docs/payments/checkout/fulfillment
-        successUrl: window.location.protocol + '//https://hopeful-meitner-cb550e.netlify.com/#/',
-        cancelUrl: window.location.protocol + '//https://hopeful-meitner-cb550e.netlify.com/#/landing',
+        successUrl: window.location.protocol + '//hopeful-meitner-cb550e.netlify.com/#/',
+        cancelUrl: window.location.protocol + '//hopeful-meitner-cb550e.netlify.com/#/landing',
     })
     .then(function (result) {
       if (result.error) {
